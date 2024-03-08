7e852998ecbc63c987cac0d23885221e355be245
H31M44\Student <Student>
2024-03-08 17:25:09 +0700

    init

---
+++ sv02_jwt/src/main/java/com/example/sv02_jwt/Sv02JwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.sv02_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sv02JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sv02JwtApplication.class, args);
+	}
+
+}
---
+++ sv02_jwt/src/main/java/model/Role.java
@@ -0,0 +1,5 @@
+package model;
+
+public enum Role {
+	ROLE_USER, ROLE_ADMIN
+}
